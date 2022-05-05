9cbfefb9a9d7560fc025ab540b86d1fd5ab3af57
shiyi <sydryy@gmail.com>
2022-05-05 08:35:28 +0800

    encrypt string

--- src/solitaire_cipher.rb
+++ src/solitaire_cipher.rb
@@ -7,12 +7,13 @@ class SolitaireCipher
 
   def transform_number_to_string(numbers)
     numbers.map do |number|
+      number -= 26 if number > 26
       (number + CONSTANT).chr
     end.join
   end
 
-  def encrypt(chars, key)
-    char_numbers = transform_string_to_number(chars)
+  def encrypt(plain_text, key)
+    char_numbers = transform_string_to_number(plain_text)
     key_numbers = transform_string_to_number(key)
     secret_numbers = char_numbers.map.with_index { |char_number, index| char_number + key_numbers[index] }
     transform_number_to_string(secret_numbers)
--- test/solitaire_cipher_test.rb
+++ test/solitaire_cipher_test.rb
@@ -2,6 +2,10 @@ require 'minitest/autorun'
 require '../src/solitaire_cipher.rb'
 
 class SolitaireCipherTest < Minitest::Test
+  def setup
+    @cipher = SolitaireCipher.new
+  end
+
   #  Key: DWJXH YRFDG TMSHP UURXJ
   #  A..Z 1 - 26
   # ASCII A - ' '
@@ -12,22 +16,27 @@ class SolitaireCipherTest < Minitest::Test
   #  C => G
   #   C => 3, D => 4
   def test_transform_string_to_number
-    cipher = SolitaireCipher.new
-    assert_equal [3], cipher.transform_string_to_number('C')
-    assert_equal [3, 4], cipher.transform_string_to_number('CD')
+    assert_equal [3], @cipher.transform_string_to_number('C')
+    assert_equal [3, 4], @cipher.transform_string_to_number('CD')
   end
 
   def test_transform_number_to_string
-    cipher = SolitaireCipher.new
-    assert_equal 'C', cipher.transform_number_to_string([3])
-    assert_equal 'CD', cipher.transform_number_to_string([3, 4])
+    assert_equal 'C', @cipher.transform_number_to_string([3])
+    assert_equal 'CD', @cipher.transform_number_to_string([3, 4])
+  end
+
+  def test_transform_number_to_string_when_number_over_26
+    assert_equal 'B', @cipher.transform_number_to_string([28])
   end
 
   #   C + D => 7
   #   7 => G
   def test_encrypt_one_char
-    cipher = SolitaireCipher.new
-    assert_equal 'G', cipher.encrypt('C', 'D')
+    assert_equal 'G', @cipher.encrypt('C', 'D')
+  end
+
+  def test_encrypt_string
+    assert_equal 'GLNCQ', @cipher.encrypt('CODEI', 'DWJXH')
   end
 
   #  CODEI => GLNCQ
