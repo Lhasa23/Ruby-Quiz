7c2b03eb9cd75f58bedf9db208eb410f02cf81cc
shiyi <sydryy@gmail.com>
2022-05-05 09:20:14 +0800

    decrypt string

--- src/solitaire_cipher.rb
+++ src/solitaire_cipher.rb
@@ -1,21 +1,39 @@
 class SolitaireCipher
   CONSTANT = 64
 
-  def transform_string_to_number(text)
+  def transform_string_to_numbers(text)
     text.bytes.map { |byte_number| byte_number - CONSTANT }
   end
 
-  def transform_number_to_string(numbers)
-    numbers.map do |number|
-      number -= 26 if number > 26
-      (number + CONSTANT).chr
-    end.join
+  def transform_numbers_to_string(numbers)
+    numbers.map(&method(:transform_number_to_char)).join
   end
 
   def encrypt(plain_text, key)
-    char_numbers = transform_string_to_number(plain_text)
-    key_numbers = transform_string_to_number(key)
-    secret_numbers = char_numbers.map.with_index { |char_number, index| char_number + key_numbers[index] }
-    transform_number_to_string(secret_numbers)
+    handle_cipher(key, plain_text) do |char_number, key_number|
+       char_number + key_number
+    end
+  end
+
+  def decrypt(secret_text, key)
+    handle_cipher(key, secret_text) do |char_number, key_number|
+      char_number - key_number
+    end
+  end
+
+  private
+
+  def transform_number_to_char(number)
+    return (number - 26 + CONSTANT).chr if number > 26
+    return (number + 26 + CONSTANT).chr if number < 0
+    (number + CONSTANT).chr
+  end
+
+  def handle_cipher(key, text)
+    char_numbers = transform_string_to_numbers(text)
+    key_numbers = transform_string_to_numbers(key)
+    secret_numbers = char_numbers.map.with_index { |char_number, index| yield(char_number, key_numbers[index]) }
+    transform_numbers_to_string(secret_numbers)
   end
 end
+
--- test/solitaire_cipher_test.rb
+++ test/solitaire_cipher_test.rb
@@ -16,17 +16,21 @@ class SolitaireCipherTest < Minitest::Test
   #  C => G
   #   C => 3, D => 4
   def test_transform_string_to_number
-    assert_equal [3], @cipher.transform_string_to_number('C')
-    assert_equal [3, 4], @cipher.transform_string_to_number('CD')
+    assert_equal [3], @cipher.transform_string_to_numbers('C')
+    assert_equal [3, 4], @cipher.transform_string_to_numbers('CD')
   end
 
   def test_transform_number_to_string
-    assert_equal 'C', @cipher.transform_number_to_string([3])
-    assert_equal 'CD', @cipher.transform_number_to_string([3, 4])
+    assert_equal 'C', @cipher.transform_numbers_to_string([3])
+    assert_equal 'CD', @cipher.transform_numbers_to_string([3, 4])
   end
 
   def test_transform_number_to_string_when_number_over_26
-    assert_equal 'B', @cipher.transform_number_to_string([28])
+    assert_equal 'B', @cipher.transform_numbers_to_string([28])
+  end
+
+  def test_transform_number_to_string_when_number_non_positive
+    assert_equal 'O', @cipher.decrypt('L', 'W')
   end
 
   #   C + D => 7
@@ -43,4 +47,14 @@ class SolitaireCipherTest < Minitest::Test
   #  COD => CODXX => encrypt message
   #  CODEI NRUBY LIVEL ONGER => GLNCQ MJAFF FVOMB JIYCB
   #
+
+  # GLNCQ => CODEI
+  #
+  def test_decrypt_one_char
+    assert_equal 'C', @cipher.decrypt('G', 'D')
+  end
+
+  def test_decrypt_string
+    assert_equal 'CODEI', @cipher.decrypt('GLNCQ', 'DWJXH')
+  end
 end
