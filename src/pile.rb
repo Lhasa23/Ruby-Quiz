e8373065f83c68f3464a3f76938fefe951a1d092
shiyi <sydryy@gmail.com>
2022-05-12 09:59:28 +0800

    create pile card

--- src/pile.rb
+++ src/pile.rb
@@ -1,9 +1,21 @@
+require_relative 'card'
+
 class Pile
-  def initialize
-    @card = Array.new(54, 'card')
+  attr_reader :cards
+
+  def initialize(cards = nil)
+    if cards.nil?
+      @cards = Array.new(52) { |index| Card.new(index % 13 + 1, Card::SUITS[index / 13]) }
+    else
+      @cards = cards
+    end
   end
 
   def card_size
-    @card.size
+    @cards.size
+  end
+
+  def card_suits
+    @cards.group_by(&:suit).keys & Card::SUITS
   end
 end
--- test/pile_test.rb
+++ test/pile_test.rb
@@ -1,17 +1,32 @@
 require 'minitest/autorun'
 require '../src/pile'
+require '../src/card'
 
 class PileTest < Minitest::Test
   # Pile
-  #   cards
+  #   4 kinds of suit
+  #   include card value from 1 to 52
+  #   include two 53 of card value
   #   order!
+  #     card value from 1 to 52 and joker-a and joker-b card
   #   move_joker_a
+  #     determined joker-a card
+  #     move joker-a card to next 1 card
   def test_pile_generate_cards
-    assert_equal 54, Pile.new.card_size
+    pile = Pile.new
+    assert_equal 52, pile.card_size
+    assert_equal Card, pile.cards.first.class
   end
 
   def test_pile_has_4_suits_of_card
-    skip
-    assert_equal 4, Pile.new.card_suits.size
+    card_suits = Pile.new.card_suits
+    assert_equal 4, card_suits.size
+    assert_equal card_suits.sort, Card::SUITS.sort
+
+    assert_equal Pile.new([Card.new(1, Card::CLUB)]).card_suits, [Card::CLUB]
+  end
+
+  def test_pile_card_value_from_1_to_52
+    assert_equal (1..52).to_a, Pile.new.cards.map { |card| card.value }
   end
 end
